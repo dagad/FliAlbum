f9071e9e229eb6812621d43fa92e66d6267cfabd
devdagad <devdagad@gmail.com>
2019-04-30 08:08:43 +0900

    Initial Commit

---
+++ FliAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FliAlbum
+//
+//  Created by devdagad on 30/04/2019.
+//  Copyright © 2019 devdagad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
