5fb7d9596e5e55634ee60ff1e6ff2233e16a0d2e
dagad <dagad@estsoft.com>
2019-04-30 13:56:21 +0900

    implement server api call

---
+++ FliAlbum/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  FliAlbum
+//
+//  Created by dagad on 30/04/2019.
+//  Copyright © 2019 devdagad. All rights reserved.
+//
+
+import Mapper
+
+class Photo: Mappable {
+    required init(map: Mapper) throws {
+
+    }
+}
